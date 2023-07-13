535bcf9659521cd8c4be0135f72f7469c7c33ebe
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-13 12:06:12 -0400

    Add updated project

---
+++ lib/team.rb
@@ -0,0 +1,28 @@
+class Team 
+    attr_reader :name, :city, :roster
+    def initialize(name, city)
+        @name = name
+        @city = city
+        @roster = []
+    end
+    
+    def player_count
+        roster.length
+    end
+
+    def add_player(signed_player)
+        @roster << signed_player
+    end
+
+    def long_term_players
+        roster.find_all do |player|
+            player.contract_length > 24
+        end
+    end
+
+    def short_term_players
+        roster.find_all do |player|
+            player.contract_length <= 24
+        end
+    end
+end
